93feb01c58d23f26af8b3163cfa66b4647dbb35f
meetkpatel107205 <meetkpatel107205@gmail.com>
2025-08-13 23:29:59 +0530

    Upload All Files Till 13/08/2025

---
+++ C++/Design_a_class.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+class MyClass
+{
+    public:
+
+    void display()
+    {
+        cout << "Hello World";
+    }
+};
+
+int main()
+{
+    MyClass obj;
+
+    obj.display();
+
+    return 0;
+}
