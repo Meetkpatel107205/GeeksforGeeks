6380b259e93ef461d35445196aeaa44d2272a728
meetkpatel107205 <meetkpatel107205@gmail.com>
2025-08-22 00:07:25 +0530

    Upload All Files Till 21/08/2025

--- C++/Add_two_numbers.cpp
+++ C++/Add_two_numbers.cpp
@@ -8,6 +8,21 @@ auto add = [](int x, int y) {
     return x + y;
 };
 
+// 1. [] → Lambda introducer
+// Ye batata hai ki hum ek lambda (anonymous function) bana rahe hain.
+// Matlab aisa function jiska naam alag se define nahi kiya, direct inline likh diya.
+
+// 2. (int x, int y) → Parameters
+// Ye lambda do arguments lega: x aur y.
+
+// 3. { return x + y; } → Function body
+// Lambda ke andar humne return kiya x + y.
+// Matlab ye function dono numbers ka sum karega.
+
+// 4. auto add = ...;
+// Humne is lambda ko ek variable add me store kar liya.
+// Ab add ek callable function object ban gaya jisko hum normal function ki tarah call kar sakte hain.
+
 int main() {
     int x, y;
     cin >> x >> y;
