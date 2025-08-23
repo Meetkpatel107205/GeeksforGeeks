4d25be54521a6982255ad578e822139e69eab9d5
meetkpatel107205 <meetkpatel107205@gmail.com>
2025-08-23 23:02:30 +0530

    Upload All Files Till 23/08/2025

---
+++ Java/Start_Coding_Java.java
@@ -0,0 +1,15 @@
+
+package Java;
+
+public class Start_Coding_Java
+{
+    static void printHelloWorld()
+    {
+        // code here
+        System.out.println("Hello World");
+    }
+    public static void main(String[] args)
+    {
+        printHelloWorld();
+    }
+}
