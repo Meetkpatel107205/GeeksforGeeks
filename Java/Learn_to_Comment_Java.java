84c7509000e8a700b6be760d33f4a55645415e5e
meetkpatel107205 <meetkpatel107205@gmail.com>
2025-08-23 23:16:47 +0530

    Upload All Files Till 23/08/2025

---
+++ Java/Learn_to_Comment_Java.java
@@ -0,0 +1,20 @@
+package Java;
+
+import java.util.Scanner;
+
+public class Learn_to_Comment_Java
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        // Prints a
+        System.out.println(a);
+        // Prints b
+        System.out.println(b);
+        // Prints c
+        System.out.println(c);
+    }
+}
