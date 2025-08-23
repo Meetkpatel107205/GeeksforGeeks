0eec7b5a2e4b33e8b13e3e30ba248c24948d7589
meetkpatel107205 <meetkpatel107205@gmail.com>
2025-08-23 22:51:54 +0530

    Upload All Files Till 23/08/2025

---
+++ Java/Decision_Making_in_Java.java
@@ -0,0 +1,36 @@
+
+package Java;
+import java.util.Scanner;
+
+public class Decision_Making_in_Java
+{
+    public static String compareNM(int n, int m) {
+        // code here
+        if(n == m)
+        {
+            return "equal";
+        }
+        else if(n < m)
+        {
+            return "lesser";
+        }
+        else
+        {
+            return "greater";
+        }
+    }
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int n,m;
+
+        System.out.print("\nEnter First Number : ");
+        n = sc.nextInt();
+
+        System.out.print("Enter Second Number : ");
+        m = sc.nextInt();
+
+        System.out.println("Relation Between Number " + n + " and Number " + m + " is : " + compareNM(n, m));
+    }    
+}
