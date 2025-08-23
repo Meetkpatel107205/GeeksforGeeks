1204c236f6dff1097e1a43c46c2e0d4d28144149
meetkpatel107205 <meetkpatel107205@gmail.com>
2025-08-23 23:12:10 +0530

    Upload All Files Till 23/08/2025

---
+++ Java/Taking_input_Java.java
@@ -0,0 +1,27 @@
+
+package Java;
+
+import java.util.Scanner;
+
+public class Taking_input_Java
+{
+    public static void main(String args[]) {
+        // Implement Scanner Class
+        Scanner sc = new Scanner(System.in);
+        
+        // Take all inputs
+        int intVal = sc.nextInt();
+        float floatVal = sc.nextFloat();
+        long longVal = sc.nextLong();
+        byte byteVal = sc.nextByte();
+        sc.nextLine();
+        String stringVal = sc.nextLine();
+        
+        // Print all the values
+        System.out.println(intVal);
+        System.out.println(floatVal);
+        System.out.println(longVal);
+        System.out.println(byteVal);
+        System.out.println(stringVal);
+    }   
+}
