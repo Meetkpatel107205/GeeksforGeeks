245eb235cf93853b068f255cf2aa4d7237980b9b
meetkpatel107205 <meetkpatel107205@gmail.com>
2025-08-24 23:44:06 +0530

    Upload All Files Till 24/08/2025

---
+++ Java/The_New_Line_Java.java
@@ -0,0 +1,17 @@
+
+package Java;
+
+public class The_New_Line_Java {
+
+    // Function to print each word in single line
+    static void printIndividualLine() {
+        System.out.println("Geeks");
+        System.out.println("for");
+        System.out.println("Geeks");
+    }
+
+    public static void main(String[] args) {
+        // Call the function
+        printIndividualLine();
+    }
+}
