c95d658a8bb134e1c7f48d173cd165896f323eff
meetkpatel107205 <meetkpatel107205@gmail.com>
2025-08-25 23:29:35 +0530

    Upload All Files Till 25/08/2025

---
+++ Java/Input_String.java
@@ -0,0 +1,20 @@
+
+package Java;
+
+import java.util.*;
+
+import java.util.Scanner;
+
+public class Input_String
+{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        String str = sc.nextLine();
+        
+        System.out.print(str);
+
+        sc.close();
+    }    
+}
