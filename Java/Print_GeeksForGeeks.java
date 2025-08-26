52484795c6b96673a9bd6bab9e463899ebead3c6
meetkpatel107205 <meetkpatel107205@gmail.com>
2025-08-26 21:44:48 +0530

    Upload All Files Till 25/08/2025

---
+++ Java/Print_GeeksForGeeks.java
@@ -0,0 +1,13 @@
+
+package Java;
+
+public class Print_GeeksForGeeks
+{
+    public static void main(String[] args)
+    {
+        // Prints Hello World
+        // System.out.println("Hello World");
+        // Prints GeeksForGeeks
+        System.out.println("GeeksForGeeks");
+    }    
+}
