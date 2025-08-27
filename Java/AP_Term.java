77a75441db201af547b73958de3dcd4c9ed856ce
meetkpatel107205 <meetkpatel107205@gmail.com>
2025-08-27 23:47:05 +0530

    Upload All Files Till 27/08/2025

---
+++ Java/AP_Term.java
@@ -0,0 +1,33 @@
+
+package Java;
+
+import java.util.Scanner;
+
+class Solution {
+    // Method to calculate nth term of A.P.
+    public int apTerm(int a, int d, int n) {
+        int ans = a + (n - 1) * d;
+        return ans;
+    }
+}
+
+public class AP_Term {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter first term (a): ");
+        int a = sc.nextInt();
+        
+        System.out.print("Enter common difference (d): ");
+        int d = sc.nextInt();
+        
+        System.out.print("Enter term number (n): ");
+        int n = sc.nextInt();
+        
+        // Create Solution object
+        Solution sol = new Solution();
+        int nthTerm = sol.apTerm(a, d, n);
+        
+        System.out.println("The " + n + "th term of A.P. is: " + nthTerm);
+    }
+}
